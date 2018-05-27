3eed0970058820b1ddbd13c2082bbee90b1fc5ab
zohac <fenrir0680@gmail.com>
2018-05-27 18:16:52 +0200

    Update the picture entity.

--- src/AppBundle/Entity/Picture.php
+++ src/AppBundle/Entity/Picture.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
