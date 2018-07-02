4ccaafb51157b66c937ce7d97aea585c7ba62095
zohac <fenrir0680@gmail.com>
2018-07-02 18:45:06 +0200

    Codacy correction.

--- src/AppBundle/Form/User/UserType.php
+++ src/AppBundle/Form/User/UserType.php
@@ -11,7 +11,6 @@ use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\Validator\Constraints\Length;
 use AppBundle\Listener\AntiSqlInjectionFormListener;
-use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
@@ -43,7 +42,6 @@ class UserType extends AbstractType
             ->add('email', EmailType::class, [
                 'constraints' => [
                     new Email(),
-                    new NotBlank(),
                 ],
             ])
             ->add('plainPassword', RepeatedType::class, [
