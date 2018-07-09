36e0da953eb759a9827f09cddc34690892ebbd0e
zohac <fenrir0680@gmail.com>
2018-07-09 19:48:28 +0200

    Update the ajax controller.

--- src/AppBundle/Controller/AjaxController.php
+++ src/AppBundle/Controller/AjaxController.php
@@ -3,12 +3,16 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Trick;
+use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class AjaxController extends Controller
 {
@@ -46,7 +50,7 @@ class AjaxController extends Controller
      *
      * @return Response
      */
-    public function listAction(Request $request, ObjectManager $entityManager): Response
+    public function listAction(Request $request, ObjectManager $entityManager, \Twig_Environment $twig): JsonResponse
     {
         // Ajax request
         if ($request->isXMLHttpRequest()) {
@@ -60,7 +64,27 @@ class AjaxController extends Controller
         // We recover all the tricks
         $listOfTricks = $entityManager->getRepository(Trick::class)->findAllWithAllEntities($limit, $offset);
 
-        // Return the view
-        return $this->render('Trick\ajax.html.twig', ['listOfTricks' => $listOfTricks]);
+        // Set token for delete link
+        foreach ($listOfTricks as $key => $trick) {
+            $csrf = $this->get('security.csrf.token_manager');
+            $trick->getUser()->setToken($csrf->refreshToken($trick->getSlug())->getValue());
+            $listOfTricks[$key] = $trick;
+        }
+
+        // Serialize the object
+        $normalizer = new ObjectNormalizer();
+        $normalizer->setCircularReferenceLimit(1);
+        // Add Circular reference handler
+        $normalizer->setCircularReferenceHandler(function ($object) {
+            return $object->getId();
+        });
+        $serializer = new Serializer([$normalizer], [new JsonEncoder()]);
+
+        // Set response
+        $response = new JsonResponse();
+        $response->setContent($serializer->serialize($listOfTricks, 'json'));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
     }
 }
